98fcf3519b0ca30c89aff250521922f07fa94c39
vasiliev-alexey <alex_v81@MAIL.RU>
2021-07-04 14:52:41 +0300

    init infrastructure

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+/* eslint-disable quotes */
+module.exports = {
+  env: {
+    es2021: true,
+    jest: true,
+  },
+
+  plugins: ['@typescript-eslint', 'react', 'eslint-plugin-import', 'prettier'],
+
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { ignoreRestSiblings: true }],
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.tsx'],
+      },
+    ],
+    'react/prop-types': 'off',
+    'no-shadow': [
+      'error',
+      { builtinGlobals: false, hoist: 'functions', allow: [] },
+    ],
+  },
+};
