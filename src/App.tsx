62c788480471736b81b9dd8e31a602412018a6f4
vasiliev-alexey <alex_v81@MAIL.RU>
2021-07-05 15:11:03 +0300

    add and  configuration loki

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HeaderComponent } from 'components/HeaderComponent';
+import { HeaderComponent } from './components/HeaderComponent';
 
 const App: React.FC = () => <HeaderComponent />;
 
